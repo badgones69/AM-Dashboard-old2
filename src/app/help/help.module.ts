bae6507ef2923e668ea71a85bce0f481b1d8509e
Florian RENARD <f-renard.externe@smacl.fr>
2022-09-14 10:49:12 +0200

    About page optimization

--- src/app/help/help.module.ts
+++ src/app/help/help.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PageAboutComponent } from './pages/page-about/page-about.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {PageAboutComponent} from './pages/page-about/page-about.component';
 import {MatDialogModule} from "@angular/material/dialog";
 
 @NgModule({
